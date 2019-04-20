f1397ab926b0e238d40d716f6e93e773d76dedcc
llamentola <llamentola@yahoo.com>
2019-04-20 11:02:34 -0700

    init msg

--- HearMeNow/ViewController.swift
+++ HearMeNow/ViewController.swift
@@ -7,14 +7,107 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate, AVAudioRecorderDelegate {
+    var hasRecording = false
+    var soundPlayer : AVAudioPlayer!
+    var soundRecorder : AVAudioRecorder!
+    var session : AVAudioSession?
+    var soundPath : String?
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var playButton: UIButton!
+    
+    @IBAction func recordPressed(_ sender: Any) {
+        if(soundRecorder?.isRecording == true)
+        {
+            soundRecorder?.stop()
+            recordButton.setTitle("Record", for: UIControl.State.normal)
+            hasRecording = true
+        } else {
+            session?.requestRecordPermission(){
+                granted in
+                if(granted == true)
+                {
+                    self.soundRecorder?.record()
+                    self.recordButton.setTitle("Stop", for: UIControl.State.normal)
+                }
+                else {
+                    print("Unable to record")
+                }
+            } }
+    }
+    
+    @IBAction func playPressed(_ sender: Any) {
+        if(soundPlayer?.isPlaying == true)
+        {
+            soundPlayer?.pause()
+            playButton.setTitle("Play", for: UIControl.State.normal)
+        }
+        else if (hasRecording == true)
+        {
+            let url = NSURL(fileURLWithPath: soundPath!)
+            //let err = error as NSError
+            //let error = NSError()
+            let error: NSError? = nil
+            
+            let soundPlayer = try! AVAudioPlayer(contentsOf: url as URL)
+            if(error == nil)
+            {
+                soundPlayer.delegate = self
+                soundPlayer.play()
+            }
+            else {
+                print("Error initializing player \(String(describing: error))")
+            }
+            playButton.setTitle("Pause", for: UIControl.State.normal)
+            hasRecording = false
+        }
+        else if (soundPlayer != nil)
+        {
+            soundPlayer?.play()
+            playButton.setTitle("Pause", for: UIControl.State.normal)
+        }
+    }
+    
+    func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool)
+    {
+        recordButton.setTitle("Record", for: UIControl.State.normal)
+    }
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        playButton.setTitle("Play", for: UIControl.State.normal)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectory = paths[0]
+        
+        let soundFilename = documentDirectory.appendingPathComponent("hearmenow.wav")
+        
+        let settings = [AVFormatIDKey : Int(kAudioFormatAppleLossless), AVEncoderAudioQualityKey : AVAudioQuality.max.rawValue, AVEncoderBitRateKey : 320000, AVNumberOfChannelsKey: 2, AVSampleRateKey: 44100.0 ] as [String : Any]
+        
+        //let audioSession = AVAudioSession.sharedInstance()
+        
+        
+        do{
+            try soundRecorder = AVAudioRecorder(url:soundFilename, settings: settings)
+            soundRecorder.prepareToRecord()
+        } catch let error as NSError {
+            print("record error:\(error.localizedDescription)")
+        }
+        
+//        if let err = error
+//        {
+//            print("Error initializing the recorder: \(err.localizedDescription)")
+//        } else {
+//            soundRecorder.delegate = self
+           
+//        }
+      
     }
-
 
 }
 
